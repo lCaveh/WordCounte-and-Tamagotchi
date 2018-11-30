7df278552432df003f7aa577316195a1c74cedab
Kaveh <lcaveh@gmail.com>
2018-11-30 10:23:57 -0800

    Project files and folder created

---
+++ WordCounter/Controllers/WordCounterController.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WordCounter.Models;
+
+namespace WordCounter.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/items")]
+    public ActionResult Index()
+    {
+     List<Item> allItems = Item.GetAll();
+      return View(allItems);
+    }
+
+    [HttpGet("/items/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/items")]
+    public ActionResult Create(string description)
+    {
+      Item myItem = new Item(description);
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost("/items/delete")]
+    public ActionResult DeleteAll()
+    {
+      Item.ClearAll();
+      return View();
+    }
+    [HttpGet("/items/{id}")]
+    public ActionResult Show(int id)
+    {
+      Item item = Item.Find(id);
+      return View(item);
+    }
+  }
+}
